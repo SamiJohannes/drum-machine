07a6de6e6f13fd480b1923dd31d8fc4d54669908
Sami Duac <39534249+SamiJohannes@users.noreply.github.com>
2020-03-27 17:05:48 -0400

    Updates

---
+++ precache-manifest.1acc9a0517f0f7573b0c8ddedc4b3199.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d37d01d1e91d664f0de9a47392b6af5",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "cee4d24b9526c9750245",
+    "url": "/drum-machine/static/css/main.563c7634.chunk.css"
+  },
+  {
+    "revision": "bcc0142f351fda2f2d62",
+    "url": "/drum-machine/static/js/2.b9af7787.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/drum-machine/static/js/2.b9af7787.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cee4d24b9526c9750245",
+    "url": "/drum-machine/static/js/main.ba8e4d50.chunk.js"
+  },
+  {
+    "revision": "8e86924d7e6604f19b80",
+    "url": "/drum-machine/static/js/runtime-main.cd17e764.js"
+  },
+  {
+    "revision": "22261813071cd8996d3de2d66d6b72b6",
+    "url": "/drum-machine/static/media/421__tictacshutup__prac-hat-2.22261813.wav"
+  },
+  {
+    "revision": "75d16f1059a7e9ca5d8bf8351b3b6bbf",
+    "url": "/drum-machine/static/media/424__tictacshutup__prac-hat-open-3.75d16f10.wav"
+  },
+  {
+    "revision": "62ee8ee91edd2cea1622005332c578e1",
+    "url": "/drum-machine/static/media/427__tictacshutup__prac-kick-light.62ee8ee9.wav"
+  },
+  {
+    "revision": "823f8796c7fdaa72058878e1febcfdd5",
+    "url": "/drum-machine/static/media/428__tictacshutup__prac-kick.823f8796.wav"
+  },
+  {
+    "revision": "085a68b39549a0cb0f40673674d18f48",
+    "url": "/drum-machine/static/media/429__tictacshutup__prac-perc-1.085a68b3.wav"
+  },
+  {
+    "revision": "3c64f65c0c9c2a858a219867bde89689",
+    "url": "/drum-machine/static/media/430__tictacshutup__prac-perc-2.3c64f65c.wav"
+  },
+  {
+    "revision": "0b2bce509e990df97961cb361fd06948",
+    "url": "/drum-machine/static/media/438__tictacshutup__prac-sidestick.0b2bce50.wav"
+  },
+  {
+    "revision": "e61e5f83af8d916cf27ce0f6fbc3fb43",
+    "url": "/drum-machine/static/media/439__tictacshutup__prac-snare-2.e61e5f83.wav"
+  },
+  {
+    "revision": "83f2a8cc99400f6397c513649152e50d",
+    "url": "/drum-machine/static/media/445__tictacshutup__prac-snare-roll-short.83f2a8cc.wav"
+  }
+]);
